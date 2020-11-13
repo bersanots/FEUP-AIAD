198fc6542ccb4c085fb238ba7154fe8811c088b5
Pedro Miguel <pedro.migas@hotmail.com>
2020-11-13 23:13:08 +0000

    dfutils

---
+++ src/agents/DFUtils.java
@@ -0,0 +1,55 @@
+import java.util.ArrayList;
+import java.util.List;
+
+import jade.core.AID;
+import jade.core.Agent;
+import jade.domain.DFService;
+import jade.domain.FIPAAgentManagement.DFAgentDescription;
+import jade.domain.FIPAAgentManagement.SearchConstraints;
+import jade.domain.FIPAAgentManagement.ServiceDescription;
+
+public final class DFUtils {
+
+    public static void register(Agent a, ServiceDescription sd)
+	{
+		DFAgentDescription dfd = new DFAgentDescription();
+		dfd.setName(a.getAID());
+
+        try {
+        	DFAgentDescription previous_entries[] = DFService.search(a, dfd);
+			if ( previous_entries.length > 0 ) 
+            	DFService.deregister(a);
+            	
+            dfd.addServices(sd);
+			DFService.register(a, dfd);
+		}
+        catch (Exception e) { 
+            e.printStackTrace(); 
+        }
+	}
+    
+	public static List<AID> getService(Agent a, String service)
+	{
+		DFAgentDescription dfd = new DFAgentDescription();
+   		ServiceDescription sd = new ServiceDescription();
+   		sd.setType(service);
+		dfd.addServices(sd);
+		
+		SearchConstraints constraints = new SearchConstraints();
+        constraints.setMaxResults((long) -1);
+        
+		try {
+			DFAgentDescription[] result = DFService.search(a, dfd, constraints);
+			List<AID> agents = new ArrayList<>();
+			
+			for (int i=0; i < result.length; i++) 
+                agents.add(result[i].getName());
+            return agents;
+		}
+		catch (Exception e) { 
+            e.printStackTrace(); 
+        }
+      	return null;
+	}
+	
+}
