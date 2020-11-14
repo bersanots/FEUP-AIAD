0737f263be79a5b7c7574c9f53bc580bcd20c1fe
Luywer <luisafonsoso@gmail.com>
2020-11-14 17:31:29 +0000

    added position updating methods

--- src/general/Position.java
+++ src/general/Position.java
@@ -9,4 +9,54 @@ public class Position {
 		this.x = x;
 		this.y = y;
 	}
+	
+	public double getDistance(Position pos)
+	{       
+	    return Math.sqrt((pos.y - this.y) * (pos.y - this.y) + (pos.x - this.x) * (pos.x - this.x));
+	}
+
+	public Position getStep(Position destination, int step_size)
+	{
+	  int diff_x = this.diffX(destination);
+	  int diff_y = this.diffY(destination);
+	  int step_x = (diff_x != 0 ? Math.abs(diff_x) / diff_x : 0) * step_size;
+	  int step_y = (diff_y != 0 ? Math.abs(diff_y) / diff_y : 0) * step_size;
+
+	  if ( (step_x > 0 && this.x + step_x > destination.x) || (step_x < 0 && this.x + step_x < destination.x) )
+	    step_x = destination.x - this.x;
+	 
+	  if ( (step_y > 0 && this.y + step_y > destination.y) || (step_y < 0 && this.y + step_y < destination.y) )
+	    step_y = destination.y - this.y;
+	    
+	  Position step = new Position(step_x, step_y);
+	  return step;
+	}
+
+	public Position getUnitaryStep(Position destination)
+	{
+	  return this.getStep(destination, 1);
+	}
+
+	private int diffY(Position pos)
+	{
+	  return pos.y - this.y;
+	}
+
+	private int diffX(Position pos)
+	{
+	  return pos.x - this.x;
+	}
+
+	@Override
+	public boolean equals(Object o) 
+	{
+	  if (this == o)
+	    return true;
+	  if (o == null)
+	    return false;
+	  if (getClass() != o.getClass())
+	    return false;
+	  Position pos = (Position) o;
+	  return this.x == pos.x && this.y == pos.y;
+	}
 }
